06dbce7dd8eac431fab75b9a37f8a3c5908cee49
Tim Thabt <haithamthabt@gmail.com>
2021-02-18 11:46:57 -0500

    Add project files.

---
+++ WebScraper/Controllers/HomeController.cs
@@ -0,0 +1,57 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using WebScraper.Models;
+
+namespace WebScraper.Controllers
+{
+    public class HomeController : Controller
+    {
+
+        Scraper scraper = new Scraper();
+
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+
+
+            scraper.ScrapeData();
+
+            var entries = scraper.Entries.ToList();
+            return View(entries);
+        }
+
+        public IActionResult GetData()
+        {
+
+
+            scraper.ScrapeData();
+
+            var entries = scraper.Entries.ToList();
+            return View(entries);
+        }
+
+
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ WebScraper/Models/Scraper.cs
@@ -0,0 +1,44 @@
+﻿using HtmlAgilityPack;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web;
+
+namespace WebScraper.Models
+{
+    public class Scraper
+    {
+        private ObservableCollection<EntryModel> _entries = new ObservableCollection<EntryModel>();
+
+        public ObservableCollection<EntryModel> Entries 
+        {
+            get { return _entries;  }
+            set { _entries = value; }
+        }
+
+        public void ScrapeData()
+        {
+            string page = "https://mmohuts.com/news/";
+            var web = new HtmlWeb();
+            var doc = web.Load(page);
+
+
+            var Articles = doc.DocumentNode.SelectNodes("//*[@class= 'article-single']");
+
+            foreach (var article in Articles) {
+                var header = HttpUtility.HtmlDecode(article.SelectSingleNode(".//li[@class = 'article-header']").InnerText);
+                var description = HttpUtility.HtmlDecode(article.SelectSingleNode(".//li[@class = 'article-copy']").InnerText);
+                Debug.Print("====================================================");
+                Debug.Print(header);
+                Debug.Print(description);
+                Debug.Print("====================================================");
+
+                _entries.Add(new EntryModel { Title = header, Description = description });
+            }
+        }
+
+    }
+}
